8b9dc659d9171350bf4f4337086f28bdde763b14
olegchabak <chabak.o@brinex.ru>
2020-06-28 16:46:02 +0300

    end https://tocode.ru/lk/courses/vue-pro/#/modals/5

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="wrapper">
+		<!--<header></header>-->
+		<div class="wrapper-content">
+			<section>
+				<div class="container">
+					<h1>{{title}}</h1>
+					<modal title="First modal" v-if="modalFirst" @modalClose="modalFirst = false">
+						<div slot="modalBody">
+							<p>test</p>
+							<button class="btn btnPrimary" @click="modalFirst = !modalFirst">Modal</button>
+						</div>
+						<div slot="modalFooter">
+							<p>Modal footer</p>
+						</div>
+					</modal>
+					<button class="btn btnPrimary" @click="modalFirst = !modalFirst">Modal</button>
+				</div>
+			</section>
+		</div>
+		<!--<footer></footer>-->
+	</div>
+</template>
+
+<script>
+	import modal from '@/components/Modal'
+	export default {
+		components: {modal},
+		data(){
+			return {
+				title: 'My modals',
+				modalFirst: false
+			}
+		},
+		methods:{}
+	}
+</script>
+
+<style>
+</style>
